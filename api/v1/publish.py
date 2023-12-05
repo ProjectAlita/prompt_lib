eb08116e235eb58a7167bdea87960b0ae0663e83
Usmonbek Ravshanov <usmonbek_ravshanov@epam.com>
2023-12-05 13:55:05 +0500

    Publish Fixes: 1. Origin fields replaced with shared_id fields in prompt 2. Variables, Messages, and Tags are being created in public 3. Duplicate prompt creation is fixed

--- api/v1/publish.py
+++ api/v1/publish.py
@@ -6,8 +6,13 @@ from tools import api_tools
 class PromptLibAPI(api_tools.APIModeHandler):
     def post(self, project_id: int, version_id: int, **kwargs):
         result = Publishing(project_id, version_id).publish()
-        status_code = 200 if result['ok'] else 400
-        return result, status_code
+        
+        if not result['ok']:
+            code = result.pop('error_code', 400)
+            return result, code
+        #
+        prompt_version = result['prompt_version']
+        return prompt_version, 200
 
 
 class API(api_tools.APIBase):
