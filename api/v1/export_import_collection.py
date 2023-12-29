7aa13766d4e1c15c52c2bd3d1f9ca3b9f3a699e1
Aspect13 <ad13box@gmail.com>
2023-12-29 10:42:13 +0100

    collection export fixes

--- api/v1/export_import_collection.py
+++ api/v1/export_import_collection.py
@@ -21,12 +21,12 @@ from ...utils.constants import PROMPT_LIB_MODE
 
 
 class PromptLibAPI(api_tools.APIModeHandler):
-    # @auth.decorators.check_api({
-    #     "permissions": ["models.prompt_lib.export_import_collection.export"],
-    #     "recommended_roles": {
-    #         c.ADMINISTRATION_MODE: {"admin": True, "editor": True, "viewer": False},
-    #         c.DEFAULT_MODE: {"admin": True, "editor": True, "viewer": False},
-    #     }})
+    @auth.decorators.check_api({
+        "permissions": ["models.prompt_lib.export_import.export"],
+        "recommended_roles": {
+            c.ADMINISTRATION_MODE: {"admin": True, "editor": True, "viewer": False},
+            c.DEFAULT_MODE: {"admin": True, "editor": True, "viewer": False},
+        }})
     def get(self, project_id: int, collection_id: int = None, **kwargs):
         to_dail = 'to_dial' in request.args
         result = collection_export(project_id, collection_id, to_dail)
@@ -45,55 +45,55 @@ class PromptLibAPI(api_tools.APIModeHandler):
     #         c.ADMINISTRATION_MODE: {"admin": True, "editor": True, "viewer": False},
     #         c.DEFAULT_MODE: {"admin": True, "editor": True, "viewer": False},
     #     }})
-    def post(self, project_id: int, **kwargs):
-        # created = []
-        # errors = []
-        created_prompts = []
-        author_id = auth.current_user().get("id")
-        
-        try:
-            imported_data = CollectionImportModel.parse_obj(request.json)
-        except Exception as e:
-            log.critical(str(e))
-            return {'error': str(e)}, 400
-
-        collection_data = imported_data.dict(exclude_unset=True)
-        prompts = collection_data['prompts']
-        collection_data["owner_id"] = project_id
-        collection_data["author_id"] = author_id
-        
-        if 'from_dial' in request.args:
-            with db.with_project_schema_session(project_id) as session:
-                for prompt_data in prompts:
-                    prompt_data["author_id"] = author_id
-                    prompt = prompts_import_from_dial(project_id, prompt_data, session)
-                    result = PromptDetailModel.from_orm(prompt)
-                    created_prompts.append(json.loads(result.json()))
-                session.commit()
-        else:
-            with db.with_project_schema_session(project_id) as session:
-                for raw in prompts:
-                    raw["owner_id"] = project_id
-                    for version in raw.get("versions", []):
-                        version["author_id"] = author_id
-                    try:
-                        prompt_data = PromptBaseModel.parse_obj(raw)
-                    except ValidationError as e:
-                        return {'ok': False, "error": e}
-                    prompt = create_prompt(prompt_data, session)
-                    session.flush()
-                    result = PromptDetailModel.from_orm(prompt)
-                    created_prompts.append(json.loads(result.json()))
-                session.commit()
-
-        collection_data["prompts"] = [
-            PromptIds(id=prompt['id'], owner_id=project_id)
-            for prompt in created_prompts
-        ]
-        new_collection = create_collection(project_id, collection_data)
-        result = new_collection.to_json()
-        result['prompts'] = created_prompts
-        return result, 201
+    # def post(self, project_id: int, **kwargs):
+    #     # created = []
+    #     # errors = []
+    #     created_prompts = []
+    #     author_id = auth.current_user().get("id")
+    #
+    #     try:
+    #         imported_data = CollectionImportModel.parse_obj(request.json)
+    #     except Exception as e:
+    #         log.critical(str(e))
+    #         return {'error': str(e)}, 400
+    #
+    #     collection_data = imported_data.dict(exclude_unset=True)
+    #     prompts = collection_data['prompts']
+    #     collection_data["owner_id"] = project_id
+    #     collection_data["author_id"] = author_id
+    #
+    #     if 'from_dial' in request.args:
+    #         with db.with_project_schema_session(project_id) as session:
+    #             for prompt_data in prompts:
+    #                 prompt_data["author_id"] = author_id
+    #                 prompt = prompts_import_from_dial(project_id, prompt_data, session)
+    #                 result = PromptDetailModel.from_orm(prompt)
+    #                 created_prompts.append(json.loads(result.json()))
+    #             session.commit()
+    #     else:
+    #         with db.with_project_schema_session(project_id) as session:
+    #             for raw in prompts:
+    #                 raw["owner_id"] = project_id
+    #                 for version in raw.get("versions", []):
+    #                     version["author_id"] = author_id
+    #                 try:
+    #                     prompt_data = PromptBaseModel.parse_obj(raw)
+    #                 except ValidationError as e:
+    #                     return {'ok': False, "error": e}
+    #                 prompt = create_prompt(prompt_data, session)
+    #                 session.flush()
+    #                 result = PromptDetailModel.from_orm(prompt)
+    #                 created_prompts.append(json.loads(result.json()))
+    #             session.commit()
+    #
+    #     collection_data["prompts"] = [
+    #         PromptIds(id=prompt['id'], owner_id=project_id)
+    #         for prompt in created_prompts
+    #     ]
+    #     new_collection = create_collection(project_id, collection_data)
+    #     result = new_collection.to_json()
+    #     result['prompts'] = created_prompts
+    #     return result, 201
 
 
 class API(api_tools.APIBase):
