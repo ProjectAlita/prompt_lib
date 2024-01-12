c6ffeaf8f35f1d5e1e650b2b73b24d0be05e0966
Aspect13 <ad13box@gmail.com>
2024-01-12 13:29:19 +0100

    fixing authors

--- api/v1/author.py
+++ api/v1/author.py
@@ -24,8 +24,10 @@ class PromptLibAPI(api_tools.APIModeHandler):
     @api_tools.endpoint_metrics
     def get(self, author_id: int, *, project_id: int):
         author: dict = get_author_data(author_id=author_id)
+        if not author:
+            return {'error': f'author with id {author_id} not found'}
         try:
-            author_project_id = self.module.context.rpc_manager.timeout(2).projects_get_personal_project_id(author.id)
+            author_project_id = self.module.context.rpc_manager.timeout(2).projects_get_personal_project_id(author['id'])
             stats = get_stats(author_project_id, author['id'])
             author.update(stats)
         except Empty:
