ceead312d314eb45c0e490cb77e51f416a126f33
Ivan Krakhmaliuk <Ivan_Krakhmaliuk@epam.com>
2024-03-20 22:00:10 +0200

    permissions for public project

--- events/config.py
+++ events/config.py
@@ -45,9 +45,6 @@ class Event:
                 payload['user_id']
             )
             #
-            ai_system_role = "system"
-            ai_admin_role = "admin"
-            ai_moderator_role = "editor"  # get from ai_public_admin_role?
             ai_project_roles = [i.strip() for i in ai_project_roles.split(',')]
             #
             user_roles = context.rpc_manager.call.admin_get_user_roles(
@@ -60,23 +57,11 @@ class Event:
                 if target_role not in user_role_names:
                     target_roles.append(target_role)
             #
-            if self.descriptor.config.get("admins_are_ai_moderators", False) and \
-                    global_admin_role in global_user_roles:
-                target_role = ai_moderator_role
-                if target_role not in user_role_names:
-                    target_roles.append(target_role)
-            #
-            if self.descriptor.config.get("admins_are_ai_admins", False) and \
-                    global_admin_role in global_user_roles:
-                target_role = ai_admin_role
-                if target_role not in user_role_names:
-                    target_roles.append(target_role)
-            #
-            if self.descriptor.config.get("admins_are_ai_systems", False) and \
-                    global_admin_role in global_user_roles:
-                target_role = ai_system_role
-                if target_role not in user_role_names:
-                    target_roles.append(target_role)
+            if global_admin_role in global_user_roles:
+                additional_roles = self.descriptor.config.get("add_admin_roles", [])
+                for target_role in additional_roles:
+                    if target_role not in user_role_names:
+                        target_roles.append(target_role)
             #
             log.info(
                 'Adding AI user %s to project %s with new roles %s',
--- events/init.py
+++ events/init.py
@@ -14,19 +14,84 @@ class Event:
         #
         if self.descriptor.config.get("auto_setup", False):
             log.info("Performing post-init setup checks")
-            # Check and create secrets
-            vault_client = VaultClient()
-            secrets = vault_client.get_all_secrets()
-            secrets_changed = False
+            # Data
+            setup_roles = {
+                "prompt_lib_moderators": [
+                    "models.prompt_lib.approve.post",
+                    "models.prompt_lib.approve_collection.post",
+                    "models.prompt_lib.reject.post",
+                    "models.prompt_lib.reject_collection.delete",
+                    "models.prompts",
+                    "projects.projects.project.view"
+                ],
+                "prompt_lib_public": [
+                    "models.datasource",
+                    "models.datasources",
+                    "models.datasources.dataset.details",
+                    "models.datasources.datasource.details",
+                    "models.datasources.predict.post",
+                    "models.datasources.public_datasources.list",
+                    "models.datasources.search.post",
+                    "models.datasources.version.details",
+                    "models.datasources.versions.get",
+                    "models.prompt_lib",
+                    "models.prompt_lib.author.detail",
+                    "models.prompt_lib.collection.details",
+                    "models.prompt_lib.collections.list",
+                    "models.prompt_lib.export_import.export",
+                    "models.prompt_lib.export_import.import",
+                    "models.prompt_lib.predict.post",
+                    "models.prompt_lib.prompt.details",
+                    "models.prompt_lib.prompts.list",
+                    "models.prompt_lib.prompts.search",
+                    "models.prompt_lib.public_collection.details",
+                    "models.prompt_lib.public_prompt.details",
+                    "models.prompt_lib.public_prompts.list",
+                    "models.prompt_lib.search_requests.list",
+                    "models.prompt_lib.tags.list",
+                    "models.prompt_lib.trending_authors.list",
+                    "models.prompt_lib.version.details",
+                    "models.prompts",
+                    "projects.projects.project.view"
+                ],
+                "public_admin": [
+                    "models.datasource",
+                    "models.datasource.publish.post",
+                    "models.datasource.unpublish.delete",
+                    "models.datasources",
+                    "models.datasources.dataset.delete",
+                    "models.datasources.dataset.details",
+                    "models.datasources.dataset.update",
+                    "models.datasources.dataset_status.details",
+                    "models.datasources.datasource.delete",
+                    "models.datasources.datasource.details",
+                    "models.datasources.datasources.create",
+                    "models.datasources.datasources.list",
+                    "models.datasources.datasources.update",
+                    "models.datasources.deduplicate.post",
+                    "models.datasources.predict.post",
+                    "models.datasources.public_datasources.list",
+                    "models.datasources.search.post",
+                    "models.datasources.version.delete",
+                    "models.datasources.version.details",
+                    "models.datasources.version.update",
+                    "models.datasources.versions.create",
+                    "models.datasources.versions.get"
+                ]
+            }
             #
             setup_secrets = {
                 "ai_project_allowed_domains": self.descriptor.config.get(
                     "ai_project_allowed_domains", ""
                 ),
-                "ai_project_roles": "viewer",
-                "ai_public_admin_role": "editor",
+                "ai_project_roles": "prompt_lib_public",  # change to viewer after redo
+                "ai_public_admin_role": "public_admin",  # change to editor after redo
                 "ai_project_api_url": "",
             }
+            # Check and create secrets
+            vault_client = VaultClient()
+            secrets = vault_client.get_all_secrets()
+            secrets_changed = False
             #
             for key, value in setup_secrets.items():
                 if key not in secrets:
@@ -53,10 +118,23 @@ class Event:
                     plugins=["configuration", "models"],
                     admin_email=system_user,
                     owner_id=system_user_id,
-                    roles=["editor", "viewer"],
+                    roles=["system"],
                 )
                 #
                 if public_project_id is not None:
+                    # Apply correct permissions
+                    for role, permissions in setup_roles.items():
+                        self.context.rpc_manager.call.admin_add_role(
+                            project_id=public_project_id,
+                            role_names=[role],
+                        )
+                        #
+                        self.context.rpc_manager.call.admin_set_permissions_for_role(
+                            project_id=public_project_id,
+                            role_name=role,
+                            permissions=permissions,
+                        )
+                    # Save ID to secrets
                     secrets["ai_project_id"] = public_project_id
                     secrets_changed = True
             # Save secrets if changes are made
