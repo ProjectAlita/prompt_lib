a91317ba846795a18c61a280049fcd8e3565191b
Mikhail Campos Guadamuz <Mikhail_Campos-Guadamuz@epam.com>
2024-11-01 17:08:27 +0300

    Support of importing applications with complex application, datasource and prompt tools structure

--- api/v1/import_wizard.py
+++ api/v1/import_wizard.py
@@ -1,10 +1,13 @@
-from flask import request, send_file
+from typing import List
 
-from tools import api_tools, db, auth, config as c
+from flask import request
+
+from tools import api_tools, auth, config as c, rpc_tools
 
 from pydantic import ValidationError
+from pydantic.utils import deep_update
 
-from ...models.pd.import_wizard import IMPORT_MODEL_ENTITY_MAPPER
+from ...models.pd.import_wizard import ApplicationImportCompoundTool, IMPORT_MODEL_ENTITY_MAPPER
 from ...utils.constants import PROMPT_LIB_MODE
 
 
@@ -13,6 +16,28 @@ ENTITY_IMPORT_MAPPER = {
     'datasources': 'datasources_import_datasource',
     'agents': 'applications_import_application',
 }
+
+
+def postponed_app_tools_import(postponed_application_tools: List[ApplicationImportCompoundTool], postponed_id_mapper, project_id):
+    rpc_call = rpc_tools.RpcMixin().rpc.call
+
+    errors = []
+    results = {}
+    for tool in postponed_application_tools:
+        app_ver_id, payload = tool.generate_create_payload(postponed_id_mapper)
+        result = rpc_call.applications_add_application_tool(
+            payload,
+            project_id,
+            app_ver_id,
+            return_details=True
+        )
+        if not result['ok']:
+            errors.append(result['error'])
+        else:
+            # the most recent tools update overwrites result with the most actual data
+            results[app_ver_id] = result['details']
+
+    return results.values(), errors
 
 
 class PromptLibAPI(api_tools.APIModeHandler):
@@ -32,25 +57,52 @@ class PromptLibAPI(api_tools.APIModeHandler):
             result[key] = []
             errors[key] = []
 
+        # tools deleted from initial application importing
+        # to add separetly, when all initial app/ds/prompts are imported
+        postponed_application_tools = []
+        # map exported import_uuid/import_version_uuid with real id's of saved entities
+        postponed_id_mapper = {}
         for item in import_data:
+            postponed_tools = []
             entity = item['entity']
             entity_model = IMPORT_MODEL_ENTITY_MAPPER.get(entity)
             if entity_model:
                 try:
-                    model = entity_model.parse_obj(item).dict()
+                    model = entity_model.parse_obj(item)
                 except ValidationError as e:
                     return {'errors': {entity: [f'Validation error on item {entity}: {e}']}}, 400
             else:
                 return {'errors': {entity: [f'No such {entity} in import entity mapper']}}, 400
 
+            if entity == 'agents':
+                postponed_tools = model.postponed_tools
+                postponed_application_tools.extend(postponed_tools)
+
+            model_data = model.dict()
+
             rpc_func = ENTITY_IMPORT_MAPPER.get(entity)
             if rpc_func:
                 r, e = getattr(self.module.context.rpc_manager.call, rpc_func)(
-                    model, project_id, author_id
+                    model_data, project_id, author_id
                 )
                 if r:
-                    result[entity].append(r)
+                    # result will be appended later when all tools will be added to agent
+                    if not postponed_tools:
+                        result[entity].append(r)
+                    postponed_id_mapper = deep_update(
+                        postponed_id_mapper,
+                        model.map_postponed_ids(imported_entity=r)
+                    )
                 errors[entity].extend(e)
+
+        if postponed_application_tools:
+            tool_results, tool_errors = postponed_app_tools_import(
+                postponed_application_tools,
+                postponed_id_mapper,
+                project_id
+            )
+            result['agents'].extend(tool_results)
+            errors['agents'].extend(tool_errors)
 
         has_results = any(result[key] for key in result if result[key])
         has_errors = any(errors[key] for key in errors if errors[key])
--- models/pd/import_wizard.py
+++ models/pd/import_wizard.py
@@ -1,27 +1,128 @@
-from typing import List, Optional
+from typing import List, Literal, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, root_validator
 
 
 class ImportData(BaseModel):
+    import_uuid: str
     entity: str
     name: str
     description: str
     is_selected: bool = False
 
+    def map_postponed_ids(self, imported_entity):
+        return {}
+
+    class Config:
+        fields = {
+            'import_uuid': {'exclude': True},
+        }
+
 
 class PromptImport(ImportData):
     versions: List[dict]
-    collection_id: Optional[int]
+
+    def dict(self, **kwargs):
+        res = super().dict(**kwargs)
+        for v in res['versions']:
+            v.pop('import_version_uuid', None)
+
+        return res
+
+    def map_postponed_ids(self, imported_entity):
+        postponed_id_mapper = {
+            'prompt_id': {
+                self.import_uuid: imported_entity['id']},
+            'prompt_version_id': {},
+        }
+
+        for version in self.versions:
+            for imported_version in imported_entity['versions']:
+                # find by unique version name within one entity id
+                if version['name'] == imported_version['name']:
+                    postponed_id_mapper['prompt_version_id'][version['import_version_uuid']] = imported_version['id']
+                    break
+
+        return postponed_id_mapper
 
 
 class DatasourcesImport(ImportData):
     versions: List[dict]
-    collection_id: Optional[int]
     embedding_model: str
     embedding_model_settings: dict
     storage: Optional[str]
     storage_settings: Optional[dict] = {}
+
+    def map_postponed_ids(self, imported_entity):
+        postponed_id_mapper = {
+            'datasource_id': {
+                self.import_uuid: imported_entity['id']
+            },
+        }
+
+        return postponed_id_mapper
+
+
+class PromptImportToolSettings(BaseModel):
+    prompt_id: int
+    prompt_version_id: int
+    variables: List[dict]
+
+
+class SelfImportToolSettings(BaseModel):
+    import_uuid: str
+    import_version_uuid: str
+    variables: List[dict]
+
+
+class DatasourceImportToolSettings(BaseModel):
+    datasource_id: int
+    selected_tools: dict
+
+
+class DatasourceSelfImportToolSettings(BaseModel):
+    import_uuid: str
+    selected_tools: list
+
+
+class ApplicationImportToolSettings(BaseModel):
+    application_id: int
+    application_version_id: int
+    variables: List[dict]
+
+
+class ApplicationImportCompoundTool(BaseModel):
+    name: str
+    description: Optional[str]
+    type: Literal['application', 'datasource', 'prompt']
+    application_import_version_uuid: Optional[str] = None
+    settings: PromptImportToolSettings | SelfImportToolSettings | DatasourceImportToolSettings | DatasourceSelfImportToolSettings | ApplicationImportToolSettings
+
+    @property
+    def not_imported_yet_tool(self):
+        return hasattr(self.settings, 'import_uuid')
+
+    def generate_create_payload(self, postponed_id_mapper):
+        assert self.application_import_version_uuid is not None
+
+        tool = self.dict()
+
+        app_ver_id = postponed_id_mapper['application_version_id'].get(
+            self.application_import_version_uuid
+        )
+
+        tool_type = tool['type']
+        tool_id_key = f'{tool_type}_id'
+        tool_version_id_key = f'{tool_type}_version_id'
+        tool['settings'][tool_id_key] = postponed_id_mapper[tool_id_key].get(
+            tool['settings'].pop('import_uuid')
+        )
+        if tool['settings'].get('import_version_uuid'):
+            tool['settings'][tool_version_id_key] = postponed_id_mapper[tool_version_id_key].get(
+            tool['settings'].pop('import_version_uuid')
+        )
+
+        return app_ver_id, tool
 
 
 class AgentsImport(ImportData):
@@ -29,6 +130,56 @@ class AgentsImport(ImportData):
     owner_id: Optional[int]
     shared_id: int = None
     shared_owner_id: int = None
+
+    postponed_tools: List[ApplicationImportCompoundTool] = []
+
+    class Config:
+        fields = {
+            'import_uuid': {'exclude': True},
+            'postponed_tools': {'exclude': True},
+        }
+
+    @root_validator(pre=True)
+    def validate_compound_tool(cls, values):
+        postponed_tools = []
+        for version in values['versions']:
+            clean_tools = []
+            for tool in version.get('tools', []):
+                if tool['type'] in ('application', 'datasource', 'prompt'):
+                    t = ApplicationImportCompoundTool.parse_obj(tool)
+                    if t.not_imported_yet_tool:
+                        t.application_import_version_uuid = version['import_version_uuid']
+                        postponed_tools.append(t)
+                    else:
+                        clean_tools.append(tool)
+                else:
+                    clean_tools.append(tool)
+
+            version['tools'] = clean_tools
+        values['postponed_tools'] = postponed_tools
+
+        return values
+
+    def map_postponed_ids(self, imported_entity: dict):
+        ''' Map import_uuid with real id/version_id of app stored in db'''
+
+        postponed_id_mapper = {
+            'application_id': {
+                self.import_uuid: imported_entity['id']
+            },
+            'application_version_id': {},
+        }
+
+        for version in self.versions:
+            for imported_version in imported_entity['versions']:
+                # find by unique version name within one entity id
+                if version['name'] == imported_version['name']:
+                    postponed_id_mapper['application_version_id'][version['import_version_uuid']] = imported_version['id']
+                    break
+
+        return postponed_id_mapper
+
+
 
 
 IMPORT_MODEL_ENTITY_MAPPER = {
